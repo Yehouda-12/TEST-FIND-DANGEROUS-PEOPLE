23d16f25e98bb9132827db7ea247a4011422c5df
Yehouda-12 <coheny748@gmail.com>
2025-12-17 10:11:30 +0200

    load data

--- utils.js
+++ utils.js
@@ -1,1 +1,32 @@
-const url = `https://spies-test-server.vercel.app/`
+import fs from "node:fs";
+const url = `https://spies-test-server.vercel.app/`;
+
+export async function getListFromServer(pathParam) {
+  const url = `https://spies-test-server.vercel.app/${pathParam}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function writeToJson(text, nameFile) {
+  fs.writeFile(nameFile, text, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("File written successfully");
+  });
+}
+
+export async function getPeopleAndWrite(nameFile) {
+  const text = await getListFromServer("/people");
+  writeToJson(text, nameFile);
+}
+export async function getrecordAndWrite(nameFile) {
+  const text = await getListFromServer("/transcriptions");
+  writeToJson(text, nameFile);
+}
